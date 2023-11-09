140519ee56f2c39980afa0bb5f7d06b90b444895
FerAma20 <ferama22@gmail.com>
2023-11-08 22:55:01 -0600

    se agredo la direccion del portafolio

--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
     <footer className="footer_ludent" >
     <div >
-      <span className="text_footer" >© 2023 <a href="#" className="hover:underline">Fernando Escobar™</a>. All Rights Reserved.
+      <span className="text_footer" >© 2023 <a href="https://sweet-brioche-15580e.netlify.app/" className="hover:underline" target="_blank">Fernando Escobar™</a>. All Rights Reserved.
     </span>
     
     </div>
